f81d211166b4567bdac99f79540e6b9611bc1a08
Xiaowei Chen <xiaoqianniu@gmail.com>
2023-06-27 16:55:32 -0400

    version6: change MainApp.kt for pulling from dongguo

--- shared/src/commonMain/kotlin/com/lduboscq/appkickstarter/MainApp.kt
+++ shared/src/commonMain/kotlin/com/lduboscq/appkickstarter/MainApp.kt
@@ -4,17 +4,7 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.transitions.SlideTransition
-import com.lduboscq.appkickstarter.list.ListScreenContent
-import com.lduboscq.appkickstarter.list.PersonsListScreen
-import com.lduboscq.appkickstarter.xiao_login.PreLoginScreen
 import com.dishdiscoverers.foodrecipe.z_showList.HomeScreen
-//@OptIn(ExperimentalAnimationApi::class)
-//@Composable
-//public fun MainApp() {
-//    Navigator(PreLoginScreen()) { navigator ->
-//        SlideTransition(navigator)
-//    }
-//}
 
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
