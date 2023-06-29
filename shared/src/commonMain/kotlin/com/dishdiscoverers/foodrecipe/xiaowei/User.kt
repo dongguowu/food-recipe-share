e3925426c86a74d46ad2b704d310de03e8c0d84a
Xiaowei Chen <xiaoqianniu@gmail.com>
2023-06-29 10:36:51 -0400

    version18: wrong version, error inside after implementation in gradle

--- shared/src/commonMain/kotlin/com/dishdiscoverers/foodrecipe/xiaowei/User.kt
+++ shared/src/commonMain/kotlin/com/dishdiscoverers/foodrecipe/xiaowei/User.kt
@@ -3,6 +3,7 @@ package com.dishdiscoverers.foodrecipe.xiaowei
 import io.realm.kotlin.types.RealmObject
 import io.realm.kotlin.types.RealmUUID
 import io.realm.kotlin.types.annotations.PrimaryKey
+import kotlinx.serialization.Serializable
 
 
 class User : RealmObject {
@@ -14,13 +15,12 @@ class User : RealmObject {
     var confirmPassword: String = ""
 }
 
-
+@Serializable
 data class UserData(
     var id: String? = null,
     var username: String = "",
     var email: String = "",
     var password: String = "",
     var confirmPassword: String = "",
-    var listOfUsers : ArrayList<User>? = null,
-    var user: User?
+
 )
