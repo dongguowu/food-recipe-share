ac8f5c808740cd24158210433ae31f80d2be6c17
Dongguo WU <david.dong.guo@gmail.com>
2023-06-29 06:20:49 -0400

    local firebase

--- shared/src/commonMain/kotlin/com/dishdiscoverers/foodrecipe/z_showList/RecipeScreenModel.kt
+++ shared/src/commonMain/kotlin/com/dishdiscoverers/foodrecipe/z_showList/RecipeScreenModel.kt
@@ -2,7 +2,9 @@ package com.dishdiscoverers.foodrecipe.z_showList
 
 import cafe.adriel.voyager.core.model.StateScreenModel
 import cafe.adriel.voyager.core.model.coroutineScope
+import com.dishdiscoverers.foodrecipe.dongguo.AuthRepository
 import com.dishdiscoverers.foodrecipe.dongguo.Category
+import com.dishdiscoverers.foodrecipe.dongguo.FirebaseUser
 import com.dishdiscoverers.foodrecipe.dongguo.Recipe
 import com.dishdiscoverers.foodrecipe.dongguo.RecipeRepository
 import com.dishdiscoverers.foodrecipe.dongguo.Resource
@@ -12,8 +14,8 @@ import kotlinx.coroutines.launch
 
 class RecipeScreenModel(
     private val localRepository: RecipeRepository,
-    private val secondLocalRepository: RecipeRepository,
     private val apiRepository: RecipeRepository,
+    private val authRepository: AuthRepository
 ) :
     StateScreenModel<RecipeScreenModel.State>(State.Init) {
 
@@ -23,16 +25,51 @@ class RecipeScreenModel(
         data class Result(val list: List<Recipe>) : State()
     }
 
+    // Auth
+    private val _loginFlow = MutableStateFlow<Resource<FirebaseUser>?>(null)
+    val loginFlow: StateFlow<Resource<FirebaseUser>?> = _loginFlow
+
+    private val _signupFlow = MutableStateFlow<Resource<FirebaseUser>?>(null)
+    val signupFlow: StateFlow<Resource<FirebaseUser>?> = _signupFlow
+
+    val currentUser: FirebaseUser? get() = authRepository.currentUser
+
+    // Category
     private val _categories = MutableStateFlow<Resource<List<Category>>?>(null)
     val categories: StateFlow<Resource<List<Category>>?> = _categories
 
     init {
+        if (authRepository.currentUser != null) {
+            _loginFlow.value = Resource.Success(authRepository.currentUser!!)
+        }
+
         coroutineScope.launch {
             _categories.value = Resource.Loading
             _categories.value = apiRepository.getAllCategory()
         }
     }
 
+    // Auth
+    fun loginUser(email: String, password: String) = coroutineScope.launch {
+        _loginFlow.value = Resource.Loading
+        val result = authRepository.login(email, password)
+        _loginFlow.value = result
+    }
+
+    fun signupUser(name: String, email: String, password: String) = coroutineScope.launch {
+        _signupFlow.value = Resource.Loading
+        val result = authRepository.signup(name, email, password)
+        _signupFlow.value = result
+    }
+
+    fun logout() {
+        authRepository.logout()
+        _loginFlow.value = null
+        _signupFlow.value = null
+    }
+
+
+    // Recipe
     fun getAllRecipe() {
         coroutineScope.launch {
             mutableState.value = State.Loading
@@ -62,7 +99,7 @@ class RecipeScreenModel(
         coroutineScope.launch {
             mutableState.value = State.Loading
             mutableState.value =
-                State.Result(list = secondLocalRepository.searchRecipesByIngredient(title))
+                State.Result(list = localRepository.searchRecipesByIngredient(title))
         }
     }
 
