f4d67de0104823e89efd48a6bdcabe2ca593d982
Dongguo WU <david.dong.guo@gmail.com>
2023-07-02 23:31:51 -0400

    add get all ingredients, get all categories in recipe service

--- shared/src/commonMain/kotlin/com/dishdiscoverers/foodrecipe/dongguo/repository/api/RecipeServiceTheMeal.kt
+++ shared/src/commonMain/kotlin/com/dishdiscoverers/foodrecipe/dongguo/repository/api/RecipeServiceTheMeal.kt
@@ -1,10 +1,13 @@
 package com.dishdiscoverers.foodrecipe.dongguo.repository.api
 
-import com.dishdiscoverers.foodrecipe.dongguo.repository.MealsResponse
+import com.dishdiscoverers.foodrecipe.dongguo.repository.Category
+import com.dishdiscoverers.foodrecipe.dongguo.repository.Ingredient
 import com.dishdiscoverers.foodrecipe.dongguo.repository.Recipe
 import com.dishdiscoverers.foodrecipe.dongguo.repository.Resource
-import com.dishdiscoverers.foodrecipe.dongguo.repository.json.convertMealRecipe
-import com.dishdiscoverers.foodrecipe.dongguo.repository.json.getIngredientsFromTheMealRecipe
+import com.dishdiscoverers.foodrecipe.dongguo.repository.json.CategoriesResponse
+import com.dishdiscoverers.foodrecipe.dongguo.repository.json.IngredientsResponse
+import com.dishdiscoverers.foodrecipe.dongguo.repository.json.convertMealCategory
+import com.dishdiscoverers.foodrecipe.dongguo.repository.json.convertMealIngredient
 import com.dishdiscoverers.foodrecipe.dongguo.repository.json.getRecipesFromTheMealJson
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
@@ -13,19 +16,17 @@ import io.ktor.client.request.get
 class RecipeServiceTheMeal(private val client: HttpClient) {
     private val THE_MEAL_API_SERVER = "https://www.themealdb.com/api/json/v1/1/"
 
+    /**
+     * Retrieve recipe from the Meal API based on the provided title.
+     * @param recipeId The id to search for in the Meal API.
+     * @return A [Recipe] retrieved from the Meal API matching the provided id, or fail if an error occurs or no recipe are found.
+     */
     suspend fun getRecipe(recipeId: String): Resource<Recipe> {
         val urlString = "${THE_MEAL_API_SERVER}lookup.php?i=$recipeId"
-        val result: MealsResponse =
-            client.get(urlString).body() ?: return Resource.Failure(Exception("Not found"))
-        val recipeTheMeal =
-            result.meals?.firstOrNull() ?: return Resource.Failure(Exception("Not found"))
-        val ingredients = getIngredientsFromTheMealRecipe(recipeTheMeal)
-        val recipe = convertMealRecipe(recipeTheMeal, ingredients)
-
-        return if (recipe != null) {
-            Resource.Success(recipe)
-        } else {
-            Resource.Failure(Exception("Conversion failed"))
+        return try {
+            Resource.Success(getRecipesFromTheMealJson(client.get(urlString).body()).first())
+        } catch (e: Exception) {
+            Resource.Failure(e)
         }
     }
 
@@ -36,20 +37,33 @@ class RecipeServiceTheMeal(private val client: HttpClient) {
      */
     suspend fun getRecipeByTitle(title: String): Resource<List<Recipe>> {
         val urlString = "${THE_MEAL_API_SERVER}search.php?s=$title"
-//        val results: MealsResponse =
-//            client.get(urlString).body() ?: return Resource.Failure(Exception("Not found"))
-//
-//        val mutableList: MutableList<Recipe> = mutableListOf()
-//        for (item in results.meals ?: emptyList()) {
-//            val ingredients = getIngredientsFromTheMealRecipe(item)
-//            convertMealRecipe(item, ingredients)?.let { mutableList.add(it) }
-//        }
-//
-//        return Resource.Success(mutableList)
-
         return try {
             Resource.Success(getRecipesFromTheMealJson(client.get(urlString).body()))
-        }catch (e: Exception) {
+        } catch (e: Exception) {
+            Resource.Failure(e)
+        }
+    }
+
+    suspend fun getAllCategory(): Resource<List<Category>> {
+        val urlString = "${THE_MEAL_API_SERVER}categories.php"
+        return try {
+            val categoriesResponse: CategoriesResponse = client.get(urlString).body()
+            val categories =
+                categoriesResponse.categories?.mapNotNull { convertMealCategory(it) } ?: emptyList()
+            Resource.Success(categories)
+        } catch (e: Exception) {
+            Resource.Failure(e)
+        }
+    }
+
+    suspend fun getAllIngredient(): Resource<List<Ingredient>> {
+        val urlString = "${THE_MEAL_API_SERVER}list.php?i=list"
+        return try {
+            val response: IngredientsResponse = client.get(urlString).body()
+            val ingredients =
+                response.meals?.mapNotNull { convertMealIngredient(it) } ?: emptyList()
+            Resource.Success(ingredients)
+        } catch (e: Exception) {
             Resource.Failure(e)
         }
     }
