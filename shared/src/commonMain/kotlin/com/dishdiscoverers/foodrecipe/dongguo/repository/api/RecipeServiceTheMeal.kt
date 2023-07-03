a0cfe5d386bfb93725ecf9e710a3c9e189711062
Dongguo WU <david.dong.guo@gmail.com>
2023-07-02 22:01:56 -0400

    update get recipes by title api server by resue from json method

--- shared/src/commonMain/kotlin/com/dishdiscoverers/foodrecipe/dongguo/repository/api/RecipeServiceTheMeal.kt
+++ shared/src/commonMain/kotlin/com/dishdiscoverers/foodrecipe/dongguo/repository/api/RecipeServiceTheMeal.kt
@@ -5,13 +5,13 @@ import com.dishdiscoverers.foodrecipe.dongguo.repository.Recipe
 import com.dishdiscoverers.foodrecipe.dongguo.repository.Resource
 import com.dishdiscoverers.foodrecipe.dongguo.repository.json.convertMealRecipe
 import com.dishdiscoverers.foodrecipe.dongguo.repository.json.getIngredientsFromTheMealRecipe
+import com.dishdiscoverers.foodrecipe.dongguo.repository.json.getRecipesFromTheMealJson
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 
-class TheMealdbApi(private val client: HttpClient) {
-    val END_POINT_GET_RECIPE = "https://www.themealdb.com/api/json/v1/1/lookup.php?i="
-    val THE_MEAL_API_SERVER = "https://www.themealdb.com/api/json/v1/1/"
+class RecipeServiceTheMeal(private val client: HttpClient) {
+    private val THE_MEAL_API_SERVER = "https://www.themealdb.com/api/json/v1/1/"
 
     suspend fun getRecipe(recipeId: String): Resource<Recipe> {
         val urlString = "${THE_MEAL_API_SERVER}lookup.php?i=$recipeId"
@@ -36,16 +36,22 @@ class TheMealdbApi(private val client: HttpClient) {
      */
     suspend fun getRecipeByTitle(title: String): Resource<List<Recipe>> {
         val urlString = "${THE_MEAL_API_SERVER}search.php?s=$title"
-        val results: MealsResponse =
-            client.get(urlString).body() ?: return Resource.Failure(Exception("Not found"))
+//        val results: MealsResponse =
+//            client.get(urlString).body() ?: return Resource.Failure(Exception("Not found"))
+//
+//        val mutableList: MutableList<Recipe> = mutableListOf()
+//        for (item in results.meals ?: emptyList()) {
+//            val ingredients = getIngredientsFromTheMealRecipe(item)
+//            convertMealRecipe(item, ingredients)?.let { mutableList.add(it) }
+//        }
+//
+//        return Resource.Success(mutableList)
 
-        val mutableList: MutableList<Recipe> = mutableListOf()
-        for (item in results.meals ?: emptyList()) {
-            val ingredients = getIngredientsFromTheMealRecipe(item)
-            convertMealRecipe(item, ingredients)?.let { mutableList.add(it) }
+        return try {
+            Resource.Success(getRecipesFromTheMealJson(client.get(urlString).body()))
+        }catch (e: Exception) {
+            Resource.Failure(e)
         }
-
-        return Resource.Success(mutableList)
     }
 }
 
