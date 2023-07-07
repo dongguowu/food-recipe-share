e25e815891289db7af8c1833b15fbf78868a8c14
Dongguo WU <david.dong.guo@gmail.com>
2023-07-07 15:43:14 -0400

    email send service

--- shared/src/commonMain/kotlin/com/lduboscq/appkickstarter/MainApp.kt
+++ shared/src/commonMain/kotlin/com/lduboscq/appkickstarter/MainApp.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.transitions.SlideTransition
+import com.dishdiscoverers.foodrecipe.dongguo.ContactUsScreen
 import com.dishdiscoverers.foodrecipe.dongguo.RecipeListScreen
 import com.dishdiscoverers.foodrecipe.xiaowei.PreLoginScreen
 
@@ -13,7 +14,8 @@ public fun MainApp() {
 //    Navigator(PersonsListScreen()) { navigator ->
 //    Navigator(PreLoginScreen()) { navigator ->
 //    Navigator(RecipeScreen()) { navigator ->
-    Navigator(RecipeListScreen()) { navigator ->
+//    Navigator(RecipeListScreen()) { navigator ->
+        Navigator(ContactUsScreen()) { navigator ->
         SlideTransition(navigator)
     }
 }
