748af01bc0900df3ea8487ae9c6a37e22cd75a20
Dongguo WU <david.dong.guo@gmail.com>
2023-08-25 10:00:08 -0400

    #3.1 recipe fedd view

--- app/src/main/java/com/dishdiscoverers/foodrecipe/presentation/RecipeFeed.kt
+++ app/src/main/java/com/dishdiscoverers/foodrecipe/presentation/RecipeFeed.kt
@@ -4,6 +4,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import com.dishdiscoverers.core.data.SaveableRecipeResource
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.tooling.preview.Preview
 import com.dishdiscoverers.core.data.RecipeResource
 
@@ -28,6 +35,15 @@ fun RecipeFeed(
 
 @Composable
 fun RecipeResourceCard(recipeResource: RecipeResource, isBookmarked: Boolean, onToggleBookmark: () -> Unit) {
+    var showDetails by rememberSaveable { mutableStateOf(false) }
+
+        ClickableText(text = AnnotatedString(recipeResource.title), onClick = {
+            showDetails = !showDetails
+        })
+
+        if (showDetails) {
+            Text(recipeResource.ingredients)
+        }
 
 }
 
