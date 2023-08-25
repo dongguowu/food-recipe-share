567779e57bc15dcb25f87b7d7ac8d5af272271f5
Dongguo WU <david.dong.guo@gmail.com>
2023-08-25 11:12:50 -0400

    #4.2 add test case for calculator

--- core/src/test/java/com/dishdiscoverers/core/utility/CalculatorTest.kt
+++ core/src/test/java/com/dishdiscoverers/core/utility/CalculatorTest.kt
@@ -1,5 +1,6 @@
 package com.dishdiscoverers.core.utility
 
+import junit.framework.TestCase.assertTrue
 import org.junit.Assert
 import org.junit.Test
 
@@ -11,10 +12,14 @@ class CalculatorTest {
         Assert.assertEquals(6, sum.toLong())
     }
 
-     @Test
+    @Test
     fun evaluatesExpressionWithFailed() {
         val calculator = Calculator()
         val sum = calculator.evaluate("1+2+3")
         Assert.assertNotEquals(7, sum.toLong())
+    }
+
+    @Test fun emailValidator_CorrectEmailSimple_ReturnsTrue() {
+        assertTrue(true)
     }
 }
