50029a84e30341a8477e7774d511a36a4dd0349e
Dongguo WU <david.dong.guo@gmail.com>
2023-08-31 11:12:49 -0400

    #5.5 display food recipe list without using DI

--- app/src/main/java/com/dishdiscoverers/foodrecipe/framework/RecipeUiState.kt
+++
@@ -1,8 +0,0 @@
-package com.dishdiscoverers.foodrecipe.framework
-
-import com.dishdiscoverers.core.data.model.SavableFoodRecipe
-
-sealed interface RecipeUiState {
-    object Loading : RecipeUiState
-    data class Success(val recipeResources : List<SavableFoodRecipe>)
-}
